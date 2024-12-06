79f6cc414b9f5564d89755eebc968dae12be998c
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-06 10:10:42 -0600

    creates randomNumber when rollDiceBtn is clicked

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let randomNumber;
+const rollDicebBtn = document.querySelector('.btn--roll');
+
+rollDicebBtn.addEventListener('click', function () {
+  randomNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(randomNumber);
+});
