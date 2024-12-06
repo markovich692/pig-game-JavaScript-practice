d7ea45fa9255c12c8f10dfe407f66dcdff3ee8e5
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-06 10:49:49 -0600

    initializes both the current and total scores values

--- script.js
+++ script.js
@@ -4,8 +4,35 @@ let randomNumber;
 const rollDicebBtn = document.querySelector('.btn--roll');
 const diceImg = document.querySelector('.dice');
 
+//CURRENT SCORES
+let firstPlayerCurrScore = document.querySelector('#current--0');
+let secondPlayerCurrScore = document.querySelector('#current--1');
+
+let firstCurrent = 0;
+let secondCurrent = 0;
+
+firstPlayerCurrScore.textContent = firstCurrent;
+secondPlayerCurrScore.textContent = secondCurrent;
+
+//TOTAL SCORES
+let firstPlayerTotalScore = document.querySelector('#score--0');
+let secondPlayerTotalScore = document.querySelector('#score--1');
+
+let firstTotal = 0;
+let secondTotal = 0;
+
+firstPlayerTotalScore.textContent = firstTotal;
+secondPlayerTotalScore.textContent = secondTotal;
+
 rollDicebBtn.addEventListener('click', function () {
   randomNumber = Math.floor(Math.random() * 6) + 1;
   diceImg.src = `dice-${randomNumber}.png`;
   diceImg.classList.remove('hidden');
+
+  if (randomNumber !== 1) {
+    firstCurrent += randomNumber;
+    firstPlayerCurrScore.textContent = firstCurrent;
+  } else {
+    console.log('it is equal 1');
+  }
 });
