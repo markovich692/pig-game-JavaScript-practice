a9ca395636f19bb5c8a845515c9d0067f067f31e
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-08 10:04:23 -0600

    starts building the game a different way

--- script.js
+++ script.js
@@ -30,80 +30,80 @@ let secondTotal = 0;
 firstPlayerTotalScore.textContent = firstTotal;
 secondPlayerTotalScore.textContent = secondTotal;
 
-rollDicebBtn.addEventListener('click', function () {
-  randomNumber = Math.floor(Math.random() * 6) + 1;
-  diceImg.src = `dice-${randomNumber}.png`;
-  diceImg.classList.remove('hidden');
+// rollDicebBtn.addEventListener('click', function () {
+//   randomNumber = Math.floor(Math.random() * 6) + 1;
+//   diceImg.src = `dice-${randomNumber}.png`;
+//   diceImg.classList.remove('hidden');
 
-  if (randomNumber !== 1 && firstTotal < 100 && secondTotal < 100) {
-    if (firstPlayer.classList.contains('player--active')) {
-      firstCurrent += randomNumber;
-      firstPlayerCurrScore.textContent = firstCurrent;
-    } else {
-      secondCurrent += randomNumber;
-      secondPlayerCurrScore.textContent = secondCurrent;
-    }
-  } else if (randomNumber === 1 && firstTotal < 100 && secondTotal < 100) {
-    if (firstPlayer.classList.contains('player--active')) {
-      firstPlayer.classList.remove('player--active');
-      secondPlayer.classList.add('player--active');
-      firstTotal += firstCurrent;
-      firstPlayerTotalScore.textContent = firstTotal;
-      firstCurrent = 0;
-      firstPlayerCurrScore.textContent = firstCurrent;
-    } else {
-      firstPlayer.classList.add('player--active');
-      secondPlayer.classList.remove('player--active');
-      secondTotal += secondCurrent;
-      secondPlayerTotalScore.textContent = secondTotal;
-      secondCurrent = 0;
-      secondPlayerCurrScore.textContent = secondCurrent;
-    }
-  } else {
-    if (firstTotal >= 100) {
-      console.log('player 1 wins');
-    } else if (secondTotal >= 100) {
-      console.log('player 2 wins');
-    }
-  }
-});
-// console.log(firstTotal);
-// console.log(secondTotal);
+//   if (randomNumber !== 1 && firstTotal < 100 && secondTotal < 100) {
+//     if (firstPlayer.classList.contains('player--active')) {
+//       firstCurrent += randomNumber;
+//       firstPlayerCurrScore.textContent = firstCurrent;
+//     } else {
+//       secondCurrent += randomNumber;
+//       secondPlayerCurrScore.textContent = secondCurrent;
+//     }
+//   } else if (randomNumber === 1 && firstTotal < 100 && secondTotal < 100) {
+//     if (firstPlayer.classList.contains('player--active')) {
+//       firstPlayer.classList.remove('player--active');
+//       secondPlayer.classList.add('player--active');
+//       firstTotal += firstCurrent;
+//       firstPlayerTotalScore.textContent = firstTotal;
+//       firstCurrent = 0;
+//       firstPlayerCurrScore.textContent = firstCurrent;
+//     } else {
+//       firstPlayer.classList.add('player--active');
+//       secondPlayer.classList.remove('player--active');
+//       secondTotal += secondCurrent;
+//       secondPlayerTotalScore.textContent = secondTotal;
+//       secondCurrent = 0;
+//       secondPlayerCurrScore.textContent = secondCurrent;
+//     }
+//   } else {
+//     if (firstTotal >= 100) {
+//       console.log('player 1 wins');
+//     } else if (secondTotal >= 100) {
+//       console.log('player 2 wins');
+//     }
+//   }
+// });
+// // console.log(firstTotal);
+// // console.log(secondTotal);
 
-holdBtn.addEventListener('click', function () {
-  if (firstPlayer.classList.contains('player--active')) {
-    firstPlayer.classList.remove('player--active');
-    secondPlayer.classList.add('player--active');
-    firstTotal += firstCurrent;
-    firstPlayerTotalScore.textContent = firstTotal;
-    firstCurrent = 0;
-    firstPlayerCurrScore.textContent = firstCurrent;
-  } else {
-    firstPlayer.classList.add('player--active');
-    secondPlayer.classList.remove('player--active');
-    secondTotal += secondCurrent;
-    secondPlayerTotalScore.textContent = secondTotal;
-    secondCurrent = 0;
-    secondPlayerCurrScore.textContent = secondCurrent;
-  }
-});
+// holdBtn.addEventListener('click', function () {
+//   if (firstPlayer.classList.contains('player--active')) {
+//     firstPlayer.classList.remove('player--active');
+//     secondPlayer.classList.add('player--active');
+//     firstTotal += firstCurrent;
+//     firstPlayerTotalScore.textContent = firstTotal;
+//     firstCurrent = 0;
+//     firstPlayerCurrScore.textContent = firstCurrent;
+//   } else {
+//     firstPlayer.classList.add('player--active');
+//     secondPlayer.classList.remove('player--active');
+//     secondTotal += secondCurrent;
+//     secondPlayerTotalScore.textContent = secondTotal;
+//     secondCurrent = 0;
+//     secondPlayerCurrScore.textContent = secondCurrent;
+//   }
+// });
 
-newGameBtn.addEventListener('click', function () {
-  //Reinitializes CURRENT SCORES
-  firstCurrent = 0;
-  secondCurrent = 0;
+// newGameBtn.addEventListener('click', function () {
+//   //Reinitializes CURRENT SCORES
+//   firstCurrent = 0;
+//   secondCurrent = 0;
 
-  firstPlayerCurrScore.textContent = firstCurrent;
-  secondPlayerCurrScore.textContent = secondCurrent;
+//   firstPlayerCurrScore.textContent = firstCurrent;
+//   secondPlayerCurrScore.textContent = secondCurrent;
 
-  //Reinitializes TOTAL SCORES
-  firstTotal = 0;
-  secondTotal = 0;
+//   //Reinitializes TOTAL SCORES
+//   firstTotal = 0;
+//   secondTotal = 0;
 
-  firstPlayerTotalScore.textContent = firstTotal;
-  secondPlayerTotalScore.textContent = secondTotal;
+//   firstPlayerTotalScore.textContent = firstTotal;
+//   secondPlayerTotalScore.textContent = secondTotal;
 
-  //Undisplay the dice
+//   //Undisplay the dice
 
-  diceImg.classList.add('hidden');
-});
+//   diceImg.classList.add('hidden');
+// });
